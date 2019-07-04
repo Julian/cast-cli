e5935c94c9f529b71e2be97d3fa23ac93b0fa973
Julian Berman <Julian@GrayVines.com>
2019-07-04 13:42:11 -0400

    We can play.

--- cast_cli/_cli.py
+++ cast_cli/_cli.py
@@ -2,8 +2,24 @@ import click
 
 from cast_cli import __version__
 
+import pychromecast
 
-@click.command()
+
+@click.group()
 @click.version_option(version=__version__)
 def main():
     pass
+
+
+@main.command()
+@click.argument("url")
+@click.option("--content-type", default="video/mp4")
+def play(url, content_type):
+    """
+    Play the given URL.
+    """
+
+    chromecast, = pychromecast.get_chromecasts()
+    chromecast.wait()
+    chromecast.media_controller.play_media(url, content_type=content_type)
+    chromecast.media_controller.block_until_active()
